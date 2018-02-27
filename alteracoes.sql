fa33526d544107d78c1bbc8b11bb35daa31246d3
Fabrício <fabricio@pinaculus.com.br>
2018-02-27 14:27:29 -0300

    Modificações v1.8 e queries

--- alteracoes.sql
+++ alteracoes.sql
@@ -1,2 +1,8 @@
 update user_functionality set functionality_id = 6 where functionality_id = 24;
 update user_functionality set functionality_id = 4 where functionality_id = 22;
+
+insert into employee values (null, 'Tais Settim', 0.00, 5, 4);
+insert into employee values (null, 'Elaine Santos', 0.00, 5, 4);
+
+insert into user values (null, 'tais@thinkideias.com.br', '$2y$10$A8c0nxzzZs5LX5fpA8zmyuxHBfnt/D2lF7Klk6HePT39yNXyGvl56', 20, null);
+insert into user values (null, 'elaine@thinkideias.com.br', '$2y$10$UyEBJkNZngY3uL3UkCn1xOW1CL7hrLk8dcIN.OIGLLQoEqqEHxy52', 20, null);
