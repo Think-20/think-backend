85ec53da1a9ec4a67c1762c6432a79101953ecec
Fabrício <fabricio@pinaculus.com.br>
2018-04-19 10:34:26 -0300

    Modificações na geolocalização e horas

--- app/Timecard.php
+++ app/Timecard.php
@@ -17,12 +17,6 @@ class Timecard extends Model
         'autoExitPlace', 'autoExitPlaceCoordinates', 'entry_place_id', 'exit_place_id'
     ];
 
-    /*
-    protected $dates = [
-        'entry', 'exit'
-    ];
-    */
-
     public static function edit(array $data) {
         $id = isset($data['id']) ? (int) $data['id'] : null;
         $employeeId = isset($data['employee']['id']) ? (int) $data['employee']['id'] : null;
@@ -89,57 +83,6 @@ class Timecard extends Model
         return $officeHours;
     }
 
-    /*
-    public static function registerYourself(array $data) {
-        $approved = 0;
-        $employee = User::logged()->employee;
-        $dateEntry = new \DateTime($data['entry']);
-        $dateExit = new \DateTime($data['exit']);
-        $reason = isset($data['reason']) ? $data['reason'] : null;
-
-        $testIfExists = Timecard::where('employee_id', '=', $employee->id)
-        ->where('entry', '>=', $dateEntry->format('Y-m-d'))
-        ->where('entry', '<=', $dateEntry->format('Y-m-d') . ' 23:59:59')
-        ->count();
-
-        if($testIfExists > 0) {
-            throw new \Exception('Você já registrou nessa data.');
-        }
-
-        $interval = $dateExit->diff($dateEntry);
-        $hour = $interval->h;
-        $min = $interval->i;
-
-        $seconds = ($interval->s)
-         + ($interval->i * 60)
-         + ($interval->h * 60 * 60)
-         + ($interval->d * 60 * 60 * 24)
-         + ($interval->m * 60 * 60 * 24 * 30)
-         + ($interval->y * 60 * 60 * 24 * 365);
-
-        if(($seconds > 33300 || $seconds < 31500) && empty($reason)) {
-            throw new \Exception('Você precisa justificar a diferença de horário.');
-        } else if($seconds > 33300 || $seconds < 31500) {
-            $approved = 0;
-        } else {
-            //Aprovar, horário comercial normal
-            $approved = 1;
-        }
-        
-        $officeHours = Timecard::create(
-            array_merge($data, [
-                'employee_id' => $employee->id,
-                'reason' => $reason,
-                'approved' => $approved
-            ])
-        );
-
-        $officeHours->save();
-
-        return $officeHours;
-    }
-    */
-
     public static function statusYourself() {
         $employee = User::logged()->employee;
         return Timecard::whereNull('exit')
@@ -192,31 +135,21 @@ class Timecard extends Model
             $timecard->save();
         } else {
             $reason = isset($data['reason']) ? $data['reason'] : null;
-            $timecard->exit = new DateTime('now');
-            $interval = $timecard->exit->diff(new DateTime($timecard->entry));
+            $timecard->exit = (new DateTime('now'))->format('Y-m-d H:i:s');
+            $seconds2 = Timecard::diffInSeconds($timecard);
+            $seconds = Timecard::discount($seconds2);
 
-            $hour = $interval->h;
-            $min = $interval->i;
-
-            $seconds = ($interval->s)
-            + ($interval->i * 60)
-            + ($interval->h * 60 * 60)
-            + ($interval->d * 60 * 60 * 24)
-            + ($interval->m * 60 * 60 * 24 * 30)
-            + ($interval->y * 60 * 60 * 24 * 365);
-
-            if(($seconds > 33300 || $seconds < 31500) && empty($reason)) {
+            if($seconds > 28800 && empty($reason)) {
                 throw new \Exception('Você precisa justificar a diferença de horário.');
-            } else if(($seconds > 33300 || $seconds < 31500) && !empty($reason)) {
+            } else if($seconds > 28800 && !empty($reason)) {
                 $approved = 0;
             } else {
-                //Aprovar, horário comercial normal
                 $approved = 1;
             }
 
             $timecard->update(
                 [
-                    'exit' => (new DateTime('now'))->format('Y-m-d H:i:s'),
+                    'exit' => $timecard->exit->format('Y-m-d H:i:s'),
                     'reason' => $reason,
                     'approved' => $approved,
                     'exit_place_id' => $place_id,
@@ -262,21 +195,6 @@ class Timecard extends Model
         }
 
         return $timecards;
-
-        /*
-        if($year == null || $month == null) {
-            return;
-        }
-
-        $query = Timecard::where('entry', '>=', $year . '-' . $month . '-01 00:00:00')
-        ->where('entry', '<=', $year . '-' . $month . '-31 23:59:59');
-        
-        if($employeeId != null) {
-            $query->where('employee_id', '=', $employeeId);
-        }
-
-        return $query->get();
-        */
     }
 
     public static function showYourself(array $data) {
@@ -307,44 +225,66 @@ class Timecard extends Model
             if($timecard->approved_user != null) $timecard->approved_user->employee;
             $timecard->entry_place;
             $timecard->exit_place;
+            if($timecard->exit != null) {
+                $timecard->balance = Timecard::secondsToHourMin(Timecard::discount(Timecard::diffInSeconds(($timecard))), false);
+            }
         }
 
         return $timecards;
     }
 
-    public static function balance($employeeId) {
-        $timecards = Timecard::where('employee_id', '=', $employeeId)
-        ->whereNotNull('exit')
-        ->get();
-        $balance = 0;
+    public static function discount($seconds) {
+        /*
+        [17:18, 18/4/2018] Hugo Morales: mas juro que prefiro descontar direto...
+        [17:19, 18/4/2018] Hugo Morales: deu mais de 6:00 horas, desconta 1:00
+        deu de 4 á 6, desconta 30 min
+        [17:20, 18/4/2018] Hugo Morales: fez de mais de 12:00 desconta mais total 1:30
+        [17:20, 18/4/2018] Hugo Morales: fez mais de 14:00 desconta 2:00
+        */
+        $hours = $seconds / 3600;
+        
+        if($hours >= 4 && $hours <= 6) {
+            $seconds = $seconds - (30 * 60);
+        }
+        else if($hours > 6 && $hours <= 12) {
+            $seconds = $seconds - (60 * 60);
+        } 
+        else if($hours > 12 && $hours <= 14) {
+            $seconds = $seconds - (90 * 60);
+        }
+        else if($hours > 14) {
+            $seconds = $seconds - (120 * 60);        
+        }
 
-        $exit = null;
-        $entry = null;
-        $interval = null;
+        return $seconds;
+    }
+
+    public static function diffInSeconds(Timecard $timecard) {  
+        $exit = new DateTime($timecard->exit);
+        $interval = $exit->diff(new DateTime($timecard->entry));
+        $hour = $interval->h;
+        $min = $interval->i;
+
+        $seconds = ($interval->s)
+        + ($interval->i * 60)
+        + ($interval->h * 60 * 60)
+        + ($interval->d * 60 * 60 * 24)
+        + ($interval->m * 60 * 60 * 24 * 30)
+        + ($interval->y * 60 * 60 * 24 * 365);
+        
+        return $seconds;
+    }
+
+    public static function secondsToHourMin($seconds, $withSign = true) {
         $sign = '+';
 
-        foreach($timecards as $timecard) {
-            $entry =  new DateTime($timecard->entry);
-            $exit = new DateTime($timecard->exit);
-            $interval = $exit->diff($entry);
-
-            $seconds = ($interval->s)
-            + ($interval->i * 60)
-            + ($interval->h * 60 * 60)
-            + ($interval->d * 60 * 60 * 24)
-            + ($interval->m * 60 * 60 * 24 * 30)
-            + ($interval->y * 60 * 60 * 24 * 365);
-
-            $balance += ($seconds - 32400);
-        }
-
-        if($balance < 0) {
+        if($seconds < 0) {
             $sign = '-';
-            $balance = $balance * - 1;
+            $seconds = $seconds * - 1;
         }
             
-        $hours = floor($balance / 3600);
-        $min = floor(($balance - ($hours * 3600)) / 60);
+        $hours = floor($seconds / 3600);
+        $min = floor(($seconds - ($hours * 3600)) / 60);
 
         if(strlen($hours) == 1) { 
             $hours = '0' . $hours;
@@ -353,7 +293,22 @@ class Timecard extends Model
         if(strlen($min) == 1) { 
             $min = '0' . $min;
         }
-        return $sign . $hours . ':' . $min;
+        return $withSign ? $sign . $hours . ':' . $min : $hours . ':' . $min;
+    }
+
+    public static function balance($employeeId) {
+        $timecards = Timecard::where('employee_id', '=', $employeeId)
+        ->whereNotNull('exit')
+        ->get();
+        $balance = 0;
+
+        foreach($timecards as $timecard) {
+            $seconds2 = Timecard::diffInSeconds($timecard);
+            $seconds = Timecard::discount($seconds2);
+            $balance += $seconds;
+        }
+
+        return Timecard::secondsToHourMin($balance);
     }
 
     public static function showApprovalsPending() {
