1e8250d5559571995c692f57ef3e7315393d1b41
Weder Fabrício <weder.fabricio@hotmail.com>
2018-10-15 17:39:56 -0300

    Ao pesquisar tarefas do job, trazer junto as informações das subtarefas.

--- app/Job.php
+++ app/Job.php
@@ -618,7 +618,7 @@ class Job extends Model
 
     public function tasks() {
         return $this->hasMany('App\Task', 'job_id')->with('project_files', 'project_files.responsible', 
-        'budget', 'budget.responsible');
+        'budget', 'budget.responsible', 'task', 'task.job_activity');
     }
 
     public function creation() {
