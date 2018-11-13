0e4fb84726076607781af6338641fe2f711d1996
Fabrício <fabricio@pinaculus.com.br>
2018-11-12 22:43:34 -0200

    Ajustes para memorial descritivo, colocando itens em leiaute de tabela, com dados dinâmicos.

--- categories.sql
+++ categories.sql
@@ -1,3 +1,31 @@
+--
+-- Table structure for table `item_category`
+--
+
+DROP TABLE IF EXISTS `item_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `item_category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(50) NOT NULL,
+  `item_category_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `description` (`description`),
+  KEY `fk_itemCategoryId_item_category_id_item_category` (`item_category_id`),
+  CONSTRAINT `fk_itemCategoryId_item_category_id_item_category` FOREIGN KEY (`item_category_id`) REFERENCES `item_category` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item_category`
+--
+
+LOCK TABLES `item_category` WRITE;
+/*!40000 ALTER TABLE `item_category` DISABLE KEYS */;
+INSERT INTO `item_category` VALUES (7,'Piso',NULL),(15,'Estrutura',7),(16,'Elevação adicional',7),(17,'Revestimentos',7),(18,'MDF Laminado',17);
+/*!40000 ALTER TABLE `item_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
 DROP TABLE IF EXISTS `item`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -28,32 +56,4 @@ LOCK TABLES `item` WRITE;
 /*!40000 ALTER TABLE `item` DISABLE KEYS */;
 INSERT INTO `item` VALUES (5,'Tablado elevado á 10cm nivelado e chapeado.','Teste','sem-foto.jpg',0,15,13),(6,'Tablado elevado á 30cm nivelado e chapeado.','Teste','sem-foto.jpg',0,15,13),(7,'Praticável com elevação personalizada em Xcm','Nivelado e chapeado.','sem-foto.jpg',0,16,13),(9,'Carpete Forração (Tipos)','Teste','sem-foto.jpg',0,17,13),(10,'Grama Sintética (Tipos)','Teste','sem-foto.jpg',0,17,13),(11,'Piso Vinílico (Tipos)','Teste','sem-foto.jpg',0,17,13),(12,'Vidro Retroiluminado (Tipos)','Teste','sem-foto.jpg',0,17,13),(13,'Deck de Madeira (Tipos)','Teste','sem-foto.jpg',0,17,13),(14,'MDF Laminado Tipo 1 Cor B3 Atributo X Especial','Teste','sem-foto.jpg',0,18,13);
 /*!40000 ALTER TABLE `item` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `item_category`
---
-
-DROP TABLE IF EXISTS `item_category`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `item_category` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `description` varchar(50) NOT NULL,
-  `item_category_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `description` (`description`),
-  KEY `fk_itemCategoryId_item_category_id_item_category` (`item_category_id`),
-  CONSTRAINT `fk_itemCategoryId_item_category_id_item_category` FOREIGN KEY (`item_category_id`) REFERENCES `item_category` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `item_category`
---
-
-LOCK TABLES `item_category` WRITE;
-/*!40000 ALTER TABLE `item_category` DISABLE KEYS */;
-INSERT INTO `item_category` VALUES (7,'Piso',NULL),(15,'Estrutura',7),(16,'Elevação adicional',7),(17,'Revestimentos',7),(18,'MDF Laminado',17);
-/*!40000 ALTER TABLE `item_category` ENABLE KEYS */;
 UNLOCK TABLES;
