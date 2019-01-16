870c41590759d7dc4e022b97b7cecbdaaa595edd
Weder Fabrício <weder.fabricio@hotmail.com>
2019-01-16 15:25:56 -0200

    Ajuste para menu de administradores aparecer somente para quem tem acesso a tela de funcionários.

--- alteracoes.sql
+++ alteracoes.sql
@@ -1,21 +1,5 @@
-update functionality set url = '/employees/filter' where id = 32;
-insert into functionality values (98, '/employees/get/{id}', 'Visualizar dados de um funcionário');
-
-alter table employee add column created_at datetime default current_timestamp;
-alter table employee add column updated_at datetime on update current_timestamp;
-alter table employee add column updated_by int;
-alter table employee add constraint `fk_updated_by_employee_id_employee` foreign key (updated_by) references employee (id);
-
-update employee set updated_by = 2;
-update employee set image = 'sem-foto.jpg' where id IN (34, 32);
-
-
-update employee set name = 'Pamela' where id  = 11;
-update employee set name = 'Fabio' where id  = 12;
-update employee set name = 'Rubens' where id  = 13;
-
-insert into functionality values (99, '/employee/save', 'Cadastrar um funcionário');
-insert into functionality values (100, '/employee/edit', 'Editar um funcionário');
-insert into functionality values (101, '/employee/remove/{id}', 'Deletar um funcionário');
-
-alter table bank_account modify column account_number varchar(20) not null;
+insert into display values (34, '/employees', 'Lista de funcionários');
+insert into display values (35, '/employees/new', 'Cadastro de funcionários');
+insert into display values (36, '/employees/show/:id', 'Visualizar todas as informações de funcionários');
+insert into display values (37, '/employees/edit/:id', 'Editar funcionários');
+insert into display values (38, '/employees/list', 'Lista de funcionários');
