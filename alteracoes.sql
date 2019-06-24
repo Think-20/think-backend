7ed28090fdce773363ab8ea281b3511f395c7c8f
Weder Fabrício <weder.fabricio@hotmail.com>
2019-06-24 16:10:28 -0300

    Correcao no agendamento de orcamentos
    Ajuste no calculo do valor por dia, o procedimento estava incorreto.

--- alteracoes.sql
+++
@@ -1,4 +0,0 @@
-insert into notification_genre values (9, 'Atraso');
-insert into notification_type values (18, 'Atraso de tarefa', 9, 1);
-alter table agent add column deleted_at datetime default null;
-INSERT INTO functionality values (141, '/my-tasks/get/{id}', 'Visualizar tarefa específica como responsável', now(), now());
