5e34a151a4bc75076fc733b27304ae86924e0e41
Weder Fabrício <weder.fabricio@hotmail.com>
2019-07-17 07:22:41 -0300

    Limpar queries.

--- alteracoes.sql
+++ alteracoes.sql
@@ -1,3 +1,1 @@
-ALTER TABLE job_activity ADD COLUMN only_edit TINYINT(1) DEFAULT 0 AFTER no_params;
-INSERT INTO job_activity VALUES (15, 'Modificação de orçamento', 0, 1, 0, 1, NULL);
 #INSERT INTO job_activity VALUES (16, 'Opção de orçamento', 0, 1, 0, 0, NULL);
