fb414ae290fe78b2bc12afb5c2468d48b457b597
Fabrício Lopes <weder.fabricio@hotmail.com>
2019-11-20 22:56:32 -0300

    Adicionar atividade reunião com compartilhamento de duração com orçamentos

--- alteracoes.sql
+++ alteracoes.sql
@@ -1,6 +1,47 @@
-ALTER TABLE job_type ADD COLUMN `fixed_budget_value` DECIMAL(3,2) DEFAULT 1;
-UPDATE job_type SET `fixed_budget_value` = 1.3 WHERE description = 'Cenografia';
+INSERT INTO `companybook`.`job_activity`
+(`id`,
+`description`,
+`no_params`,
+`redirect_after_save`,
+`fixed_duration`,
+`fixed_budget_value`,
+`min_duration`,
+`max_duration`,
+`max_budget_value_per_day`,
+`max_duration_value_per_day`,
+`next_period`,
+`next_day`,
+`counter`,
+`initial`,
+`keep_responsible`,
+`visible`,
+`modification_id`,
+`option_id`,
+`min_budget_value_to_more_days`)
+VALUES
+(21,
+'Reunião',
+0,
+NULL,
+0.2,
+0,
+0,
+0,
+0,
+1,
+0,
+0,
+0,
+0,
+0,
+1,
+NULL,
+NULL,
+0);
 
-ALTER TABLE job_activity ADD COLUMN `min_budget_value_to_more_days` DOUBLE(12,2) DEFAULT 0;
-UPDATE job_activity SET `min_budget_value_to_more_days` = 200000 WHERE description IN ('Orçamento',
-'Modificação de orçamento', 'Opção de orçamento');
+INSERT INTO job_activity_employee (employee_id, job_activity_id) VALUES (21, 21);
+
+#Compartilhar duração de orçamento com reunião
+INSERT INTO job_activity_share_duration (from_id, to_id) VALUES (21, 2);
+INSERT INTO job_activity_share_duration (from_id, to_id) VALUES (2, 21);
+
