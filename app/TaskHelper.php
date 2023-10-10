7cbe58d09f5f0471d0ad24a697680396f0071009
Gabriel Zanata <gabrielzanata@bemol.com.br>
2023-10-09 19:41:38 -0600

    dd dentro do taskHelper

--- app/TaskHelper.php
+++ app/TaskHelper.php
@@ -93,6 +93,7 @@ class TaskHelper
         }
 
         if ($onlyResponsible !== null && $responsibles->filter(function ($res) use ($onlyResponsible) {
+            dd($res->id, $onlyResponsible->id);
             return $res->id == $onlyResponsible->id;
         })->count() === 0) {
             throw new Exception('Não há responsáveis para essa atividade.');
