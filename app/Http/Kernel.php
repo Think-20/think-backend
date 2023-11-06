b19f59b6a63a1af73adf9ace76e57eec0c0d6836
Gabriel Zanata <zanatag.gz@gmail.com>
2023-11-06 11:42:49 -0300

    criando middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,5 +58,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'permission' => \App\Http\Middleware\Permission::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'checkDepartment' => \App\Http\Middleware\CheckDepartment::class,
     ];
 }
