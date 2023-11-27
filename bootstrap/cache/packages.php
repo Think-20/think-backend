28c5f7fa661be61cdd5b7f8690bd09a4a0484a77
Gabriel Zanata <zanatag.gz@gmail.com>
2023-11-27 16:27:34 -0300

    teste

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,20 @@
+<?php return array (
+  'barryvdh/laravel-dompdf' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\DomPDF\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'PDF' => 'Barryvdh\\DomPDF\\Facade',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+);
