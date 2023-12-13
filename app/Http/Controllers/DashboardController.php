db784caa438c8a9fb14e155cf591a9c9a4adbf2a
Gabriel Zanata <zanatag.gz@gmail.com>
2023-12-13 19:35:49 -0300

    1

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -92,7 +92,7 @@ class DashboardController extends Controller
                     ],
                     "meta_jobs" => 1200000,
                     "meta_aprovacao" => 400000,
-                    "total" => $soma,
+                    "total" => $soma ?? 1,
                     "aprovados" => [
                         "total" => $aprovados->count,
                         "porcentagem" => round(($aprovados->count * 100) / $soma, 2),
