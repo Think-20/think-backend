a5c10de3c3dc3fc5bb69e52dc291d9a38ff84b46
Guilherme Barbosa <guilherme.barbosa@fasters.com.br>
2023-12-14 14:23:08 -0300

    c

--- app/Http/Controllers/GoalController.php
+++ app/Http/Controllers/GoalController.php
@@ -116,7 +116,7 @@ class GoalController extends Controller
     {
         $response = [];
 
-        /*for ($i = 0; $i < Carbon::parse($date_end)->diffInDays(Carbon::parse($date_init)) + 1; $i++) {
+        for ($i = 0; $i < Carbon::parse($date_end)->diffInDays(Carbon::parse($date_init)) + 1; $i++) {
 
             $dtInicio = Carbon::parse($date_init);
             $dtFim = Carbon::parse($date_init)->addDay($i);
@@ -154,7 +154,7 @@ class GoalController extends Controller
                 ]
             ];
             array_push($response, $goals);
-        }*/
+        }
 
         return $response;
     }
