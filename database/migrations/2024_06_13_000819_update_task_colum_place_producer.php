7f165bb556a068f7a3f98c6de04c916685d5c524
Guilherme Barbosa <guilherme.barbosa@fasters.com.br>
2024-06-13 19:29:33 -0300

    a

--- database/migrations/2024_06_13_000819_update_task_colum_place_producer.php
+++ database/migrations/2024_06_13_000819_update_task_colum_place_producer.php
@@ -13,8 +13,8 @@ class UpdateTaskColumnsPlaceProducer extends Migration
      */
     public function up()
     {
-        Schema::table('task', function (Blueprint $table) {
-            /*$table->double('frete_logistica')->nullable();
+        /*Schema::table('task', function (Blueprint $table) {
+            $table->double('frete_logistica')->nullable();
             $table->double('diversos_operacional')->nullable();
 
             $table->double('mezanino')->nullable();
@@ -22,12 +22,12 @@ class UpdateTaskColumnsPlaceProducer extends Migration
             $table->date('dt_inicio_event')->nullable();
             $table->date('dt_montagem')->nullable();
             $table->date('dt_fim_event')->nullable();
-            $table->date('dt_desmontagem')->nullable();*/
+            $table->date('dt_desmontagem')->nullable();
         });
 
         Schema::table('job', function (Blueprint $table) {
-            //$table->string('producer')->nullable();
-        });
+            $table->string('producer')->nullable();
+        });*/
     }
 
     /**
@@ -37,8 +37,8 @@ class UpdateTaskColumnsPlaceProducer extends Migration
      */
     public function down()
     {
-        Schema::table('task', function (Blueprint $table) {
-            /*$table->dropColumn('frete_logistica');
+        /*Schema::table('task', function (Blueprint $table) {
+            $table->dropColumn('frete_logistica');
             $table->dropColumn('diversos_operacional');
 
             $table->dropColumn('mezanino');
@@ -46,11 +46,11 @@ class UpdateTaskColumnsPlaceProducer extends Migration
             $table->dropColumn('dt_inicio_event');
             $table->dropColumn('dt_montagem');
             $table->dropColumn('dt_fim_event');
-            $table->dropColumn('dt_desmontagem');*/
-        });
+            $table->dropColumn('dt_desmontagem');
+        });*/
 
-        Schema::table('job', function (Blueprint $table) {
-            //$table->dropColumn('producer');
-        });
+        /*Schema::table('job', function (Blueprint $table) {
+            $table->dropColumn('producer');
+        });*/
     }
 }
