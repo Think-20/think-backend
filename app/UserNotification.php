7e9bb590a4a2e250854af90dca1a1b499e5ad2cf
Guilherme Barbosa <guilherme.barbosa@fasters.com.br>
2024-06-13 19:20:39 -0300

    chegou ?

--- app/UserNotification.php
+++ app/UserNotification.php
@@ -144,7 +144,6 @@ class UserNotification extends Model
 
     private static function  checkInativeClients()
     {
-
         //Cria os alertas para os cleintes do tipo agency quando já estão a mais de 3 meses sem job
         $agencyClients = FacadesDB::select(FacadesDB::raw("SELECT c.id,c.name, j1.created_at FROM client as c 
         JOIN job as j1 ON j1.client_id = c.id AND j1.created_at = (SELECT MAX(j.created_at) FROM job as j WHERE j.client_id = c.id )
