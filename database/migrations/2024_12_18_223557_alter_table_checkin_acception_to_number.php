49c58c2f20f22e6c1c93e423ba581c366fb507da
Guilherme Barbosa <guilherme.barbosa@fasters.com.br>
2024-12-19 00:35:43 -0300

    alternando o tipo das variaveis de confirmaçao do checkin para int, assim sendo possivel ter mais status alem de aceito ou não

---
+++ database/migrations/2024_12_18_223557_alter_table_checkin_acception_to_number.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableCheckinAcceptionToNumber extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('checkin', function (Blueprint $table) {
+            $table->integer('accept_client')->nullable()->change();
+            $table->integer('approval')->nullable()->change();
+
+            $table->integer('accept_proposal')->nullable()->change();
+            $table->integer('accept_production')->nullable()->change();
+
+            $table->integer('board_approval')->nullable()->change();
+            $table->integer('financial_acceptance')->nullable()->change();
+
+        });
+    }
+    public function down()
+    {   
+    }
+}
