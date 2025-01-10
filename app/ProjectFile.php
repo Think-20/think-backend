52ca57fd77e26f1ad3960249ec5b3a3bbcbbe53e
Guilherme Barbosa <guilherme.barbosa@fasters.com.br>
2025-01-10 03:09:14 -0300

    respondendo com path

--- app/ProjectFile.php
+++ app/ProjectFile.php
@@ -28,6 +28,8 @@ class ProjectFile extends Model
             }
         }
 
+        dd([sys_get_temp_dir(), $this->original_name]);
+
         if (is_file(sys_get_temp_dir() . '/' .  $this->original_name)) {
             $res = rename(sys_get_temp_dir() . '/' .  $this->original_name, $path . '/' . $this->name);
 
