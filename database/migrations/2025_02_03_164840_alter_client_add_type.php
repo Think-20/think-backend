f471c928e35cc0f23713fa22d4de195ed72ebcb6
Guilherme Barbosa <guilherme.barbosa@fasters.com.br>
2025-02-04 02:43:05 -0300

    adicionando os campos na tabela de checkin para a duo

---
+++ database/migrations/2025_02_03_164840_alter_client_add_type.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterClientAddType extends Migration
+{   
+    public function up()
+    {
+        Schema::table('client', function (Blueprint $table) {
+            $table->string('type')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('client', function (Blueprint $table) {
+            $table->dropColumn('type');
+        });
+    }
+} 
---
+++ database/migrations/2025_02_04_024227_alter_checkin_add_accept_legal_department_duo.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterCheckinAddAcceptLegalDepartmentDuo extends Migration
+{
+    public function up()
+    {
+        Schema::table('checkin', function (Blueprint $table) {
+            $table->integer('accept_legal_department')->nullable();
+            $table->integer('accept_legal_department_employee_id')->nullable();
+            $table->string('accept_legal_department_date')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('checkin', function (Blueprint $table) {
+            $table->dropColumn('accept_legal_department');
+            $table->dropColumn('accept_legal_department_employee_id');
+            $table->dropColumn('accept_legal_department_date');
+        });
+    }
+}
