1cf60699ef595147f59e56d203c67a75d49499fb
Guilherme Barbosa <guilherme.barbosa@fasters.com.br>
2025-02-04 19:41:20 -0300

    comentando mudanças no banco

--- database/migrations/2024_12_18_223557_alter_table_checkin_acception_to_number.php
+++ database/migrations/2024_12_18_223557_alter_table_checkin_acception_to_number.php
@@ -14,14 +14,14 @@ class AlterTableCheckinAcceptionToNumber extends Migration
     public function up()
     {
         Schema::table('checkin', function (Blueprint $table) {
-            $table->integer('accept_client')->nullable()->change();
-            $table->integer('approval')->nullable()->change();
+            //$table->integer('accept_client')->nullable()->change();
+            //$table->integer('approval')->nullable()->change();
 
-            $table->integer('accept_proposal')->nullable()->change();
-            $table->integer('accept_production')->nullable()->change();
+            //$table->integer('accept_proposal')->nullable()->change();
+            //$table->integer('accept_production')->nullable()->change();
 
-            $table->integer('board_approval')->nullable()->change();
-            $table->integer('financial_acceptance')->nullable()->change();
+            //$table->integer('board_approval')->nullable()->change();
+            //$table->integer('financial_acceptance')->nullable()->change();
 
         });
     }
