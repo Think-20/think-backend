e2c523981aeeec628133ef3b0278d4b077a3a6d9
Guilherme Barbosa <guilherme.barbosa@fasters.com.br>
2025-02-05 09:58:45 -0300

    Alterando validação de extra

--- app/Http/Controllers/ExtraController.php
+++ app/Http/Controllers/ExtraController.php
@@ -15,7 +15,7 @@ class ExtraController extends Controller
 
     public function selectExtra(Request $request, int $id = null)
     {
-        if ($id != null) {
+        if ($id == null) {
             $extra = Extra::list();
             if (!$extra) {
                 return response()->json(['error' => 'true', 'message' => 'Nenhum extra encontrado'], 400);
